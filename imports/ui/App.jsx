d83329a29a78d9be33e141f3be81479fd16beb4c
samwoodson <samuelwoodson@gmail.com>
2016-06-07 16:09:56 -0700

    toggle show/hide completed, added ios and android platforms

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,6 +7,14 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false
+    };
+  }
+
   handleSubmit(e) {
     e.preventDefault();
 
@@ -19,8 +27,19 @@ class App extends Component {
 
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key= {task._id} task={task} />
     ));
   }
@@ -30,6 +49,16 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
