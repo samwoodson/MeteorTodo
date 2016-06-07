d48897f9e2dc604ce3e3441d4f1d9d18e757e202
samwoodson <samuelwoodson@gmail.com>
2016-06-07 16:27:49 -0700

    added task counter

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -48,7 +48,7 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
 
           <label className="hide-completed">
             <input
@@ -79,10 +79,12 @@ class App extends Component {
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired,
 };
 
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 }, App);
